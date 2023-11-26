a9bef08286e81f24da3c3960743771af02f0e3d1
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-11-26 14:39:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clampy
+
+Clampy is a small package written in Python3 to clamp values.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Clampy",
+    version="1.0.0",
+    packages=find_packages(),
+    author="LittleBread69",
+    author_email="chelouminatis+pypi@gmail.com",
+    description="Clamp your vars",
+    url=""
+    license="MIT",
+)
